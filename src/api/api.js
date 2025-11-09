860d0409547b6833311f5db4beaf72ec1b38a794
subramaniyanjeyaraj <subramaniyanj9363@gmail.com>
2025-11-09 11:37:01 +0530

    Initial commit for frontend

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://productcatalog-3-m2rz.onrender.com/api'
 });
+
 export default api;
